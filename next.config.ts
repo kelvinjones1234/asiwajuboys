f2883e82f04ff02b1926701ab699e58d60e7ffc0
kelvinjones1234 <praisejournal2@gmail.com>
2026-03-06 16:17:45 +0100

    max size

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // Set this to your desired limit
+    },
+  },
 };
 
 export default nextConfig;
